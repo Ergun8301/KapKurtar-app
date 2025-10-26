cac3c21647261bbe2f7ca7b3ec5c741b02cb145a
Ergun8301 <kilicergun01@gmail.com>
2025-10-26 11:53:24 +0100

    Updated OffersPage.tsx

--- src/pages/OffersPage.tsx
+++ src/pages/OffersPage.tsx
@@ -450,4 +450,31 @@ export default function OffersPage() {
                       <span className="line-through text-gray-400 text-sm">{o.price_before.toFixed(2)} €</span>
                     </div>
                   </div>
-                </
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export function createGeoJSONCircle(center: [number, number], radiusInMeters: number, points = 64) {
+  const coords = { latitude: center[1], longitude: center[0] };
+  const km = radiusInMeters / 1000;
+  const ret: [number, number][] = [];
+
+  const distanceX = km / (111.32 * Math.cos((coords.latitude * Math.PI) / 180));
+  const distanceY = km / 110.574;
+
+  for (let i = 0; i < points; i++) {
+    const theta = (i / points) * (2 * Math.PI);
+    const x = distanceX * Math.cos(theta);
+    const y = distanceY * Math.sin(theta);
+    ret.push([coords.longitude + x, coords.latitude + y]);
+  }
+
+  ret.push(ret[0]);
+  return { type: "Feature", geometry: { type: "Polygon", coordinates: [ret] } };
+}
