5ac2cbe3af8cfb5f6647debc380e38f5daa3070d
Ergun8301 <kilicergun01@gmail.com>
2025-11-05 19:11:34 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// 🚫 Retiré StrictMode pour éviter double exécution en dev
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <App />
 );
