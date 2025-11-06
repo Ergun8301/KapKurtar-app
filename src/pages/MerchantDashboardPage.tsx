c9f8378c14e2582943c16bc63dc07d6eee094a34
Ergun8301 <kilicergun01@gmail.com>
2025-11-06 15:06:30 +0100

    Updated MerchantDashboardPage.tsx

--- src/pages/MerchantDashboardPage.tsx
+++ src/pages/MerchantDashboardPage.tsx
@@ -8,7 +8,6 @@ import { useAuth } from '../hooks/useAuth';
 import { supabase } from '../lib/supabaseClient';
 import { useAddProduct } from '../contexts/AddProductContext';
 import { uploadImageToSupabase } from '../lib/uploadImage';
-import { NotificationBell } from '../components/NotificationBell';
 import { useRealtimeNotifications } from '../hooks/useRealtimeNotifications';
 import { type Notification } from '../api/notifications';
 import { OfferForm } from '../components/OfferForm';
