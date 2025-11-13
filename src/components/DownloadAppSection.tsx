2cd3f3cf0a36398645a6ae4be0364e2f756af296
Ergun8301 <kilicergun01@gmail.com>
2025-11-13 15:11:24 +0100

    Updated DownloadAppSection.tsx

--- src/components/DownloadAppSection.tsx
+++ src/components/DownloadAppSection.tsx
@@ -20,23 +20,23 @@ const DownloadAppSection = () => {
             </div>
             {/* Download Buttons */}
             <div className="flex flex-col sm:flex-row gap-4 mb-8">
-              
+              <a
                 href="#"
                 className="transition-transform hover:scale-105"
               >
-                <img 
-                  src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg" 
-                  alt="Download on the App Store" 
+                <img
+                  src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+                  alt="Download on the App Store"
                   className="h-14"
                 />
               </a>
-              
+              <a
                 href="#"
                 className="transition-transform hover:scale-105"
               >
-                <img 
-                  src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png" 
-                  alt="Get it on Google Play" 
+                <img
+                  src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+                  alt="Get it on Google Play"
                   className="h-14"
                 />
               </a>
