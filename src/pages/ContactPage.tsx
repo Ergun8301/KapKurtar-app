34dd7934eaf95b82b71596b53dee1c7e578c2380
Ergun8301 <kilicergun01@gmail.com>
2025-11-14 00:54:45 +0100

    Updated ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -230,24 +230,6 @@ const ContactPage = () => {
             </div>
           </div>
         </div>
-
-        {/* Map */}
-        <div className="mt-12">
-          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3190.9474869267363!2d31.448652!3d36.783333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14c390b0c0000000%3A0x0!2zMzbCsDQ2JzU5LjYiTiAzMcKwMjYnNTUuMSJF!5e0!3m2!1sen!2str!4v1234567890"
-              width="100%"
-              height="400"
-              style={{ border: 0 }}
-              allowFullScreen
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="TILKAPP Location"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
   );
 };
 
