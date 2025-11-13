ee04526f472d857c6a81f9e2c31d68427bb40e61
Ergun8301 <kilicergun01@gmail.com>
2025-11-14 00:29:36 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
               <img 
                 src="https://zhabjdyzawffsmvziojl.supabase.co/storage/v1/object/public/logos/Logo%20g.png" 
                 alt="TILKAPP" 
-                className="h-20 w-auto"
+                className="h-22 w-auto"
               />
             </Link>
             <p className="text-tilkapp-beige text-sm leading-relaxed">
