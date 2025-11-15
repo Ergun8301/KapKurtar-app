8e8ec0336eb0c6689d2247117a5d0020e0403959
Ergun8301 <kilicergun01@gmail.com>
2025-11-15 22:04:22 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -101,7 +101,7 @@ const Header = () => {
             <img
               src="https://zhabjdyzawffsmvziojl.supabase.co/storage/v1/object/public/logos/KK%20ORANGE%20CREME.png"
               alt="TILKAPP"
-              className="h-16 w-auto"
+              className="h-[52px] w-auto"
             />
           </a>
 
