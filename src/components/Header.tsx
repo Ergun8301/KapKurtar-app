9b0f0cc8138d868bc72ce20a35babdc665f907b6
Ergun8301 <kilicergun01@gmail.com>
2025-11-16 14:27:39 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -101,7 +101,7 @@ const Header = () => {
             <img
               src="https://zhabjdyzawffsmvziojl.supabase.co/storage/v1/object/public/logos/KK%20fond%20blanc.png"
               alt="KapKurtar"
-              className="h-20 w-auto"
+              className="h-22 w-auto"
             />
           </a>
           <div className="hidden md:block">
