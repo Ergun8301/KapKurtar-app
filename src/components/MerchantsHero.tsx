7176d6aaa931f1136bbb89f359a9806ad1c46f1d
Ergun8301 <kilicergun01@gmail.com>
2025-11-17 14:08:03 +0100

    Updated MerchantsHero.tsx

--- src/components/MerchantsHero.tsx
+++ src/components/MerchantsHero.tsx
@@ -11,7 +11,7 @@ const MerchantsHero = () => {
         <img
           src={heroImageUrl}
           alt="KapKurtar İşletmeler"
-          className="w-full h-full object-cover opacity-90"
+          className="w-full h-full object-cover opacity-20"
           onError={(e) => {
             e.currentTarget.src = fallbackImageUrl;
           }}
