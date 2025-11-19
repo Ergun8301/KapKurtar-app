f42a616e263efb8983313eaecf050cc357130a7e
Ergun8301 <kilicergun01@gmail.com>
2025-11-19 08:22:31 +0100

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -99,7 +99,7 @@ const Header = () => {
         <div className="flex justify-between items-center h-16">
           <a href="/" className="flex items-center space-x-3">
             <img
-              src="https://zhabjdyzawffsmvziojl.supabase.co/storage/v1/object/public/logos/KK%20fond%20vert%20(6).png"
+              src="https://zhabjdyzawffsmvziojl.supabase.co/storage/v1/object/public/logos/KK%20fond%20vert%20(7).png"
               alt="KapKurtar"
               className="h-10 w-auto md:h-24 lg:h-32"
             />
